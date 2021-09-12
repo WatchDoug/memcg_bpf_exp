26b68edf7f786f70fdc8320fb11f8c381634f8e3
Yutian Yang <nglaive@gmail.com>
2021-09-12 07:46:37 +0000

    update scripts

--- runv2.sh
+++ runv2.sh
@@ -2,9 +2,9 @@
 BIN_NAME=bpf
 gcc main.c -o $BIN_NAME
 mkdir ./cgroup
-sudo mount -t none cgroup2 ./cgroup
+sudo mount -t cgroup2 none ./cgroup
 sudo mkdir ./cgroup/$BIN_NAME/
 echo '1G' | sudo tee ./cgroup/$BIN_NAME/memory.max > /dev/null
 echo $$ | sudo tee ./cgroup/$BIN_NAME/cgroup.procs > /dev/null
-./$1 &
+./$BIN_NAME &
 
