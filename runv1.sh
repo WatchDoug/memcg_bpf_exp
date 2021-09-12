644afe15c8e2c98b11a7eddf5ff0da431ea1ba3b
Yutian Yang <nglaive@gmail.com>
2021-09-12 07:31:20 +0000

    update scripts

--- runv1.sh
+++ runv1.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-PID=$$
-sudo mkdir /sys/fs/cgroup/memory/$1
-sudo sh -c "echo $PID > /sys/fs/cgroup/memory/$1/cgroup.procs"
-./$1 &
+BIN_NAME=bpf
+gcc main.c -o $BIN_NAME
+sudo mkdir /sys/fs/cgroup/memory/$BIN_NAME 2>/dev/null
+echo '1G' | sudo tee /sys/fs/cgroup/memory/$BIN_NAME/memory.limit_in_bytes > /dev/null
+echo $$ | sudo tee /sys/fs/cgroup/memory/$BIN_NAME/cgroup.procs > /dev/null
+./$BIN_NAME &
 
--- runv2.sh
+++ runv2.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-PID=$$
+BIN_NAME=bpf
+gcc main.c -o $BIN_NAME
 mkdir ./cgroup
 sudo mount -t none cgroup2 ./cgroup
-sudo mkdir ./cgroup/$1/
-sudo sh -c "echo $PID > ./cgroup/$1/cgroup.procs"
+sudo mkdir ./cgroup/$BIN_NAME/
+echo '1G' | sudo tee ./cgroup/$BIN_NAME/memory.max > /dev/null
+echo $$ | sudo tee ./cgroup/$BIN_NAME/cgroup.procs > /dev/null
 ./$1 &
 
